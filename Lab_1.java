d128ef1126a1b4f7b868721fe9e31809b3427f1f
Vlad <50732247+vladkalnichenko@users.noreply.github.com>
2021-02-21 17:26:57 +0200

    Update and rename Main.java to Lab_1.java

--- Lab_1.java
+++ Lab_1.java
@@ -1,6 +1,6 @@
 package javaapplication1;
 
-public class Main {
+public class Lab_1 {
 
     public static void main(String[] args) {
         System.out.println("Кальніченко Владислав Валерійович\n122 'Комп'ютерні науки' 3 курс");
