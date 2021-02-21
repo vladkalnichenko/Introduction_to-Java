f0aac550fd7f8e34fa6f49ef33295abbef377d5e
Vlad <50732247+vladkalnichenko@users.noreply.github.com>
2021-02-21 17:26:13 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package javaapplication1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Кальніченко Владислав Валерійович\n122 'Комп'ютерні науки' 3 курс");
+    }
+}
